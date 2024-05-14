7cdf759eff8b04319f4505a7f00b7a4732d886d9
jerrodmpackard <jerrodmpackard@gmail.com>
2024-05-13 20:16:08 -0700

    added All For One image and added provision for a 4th project card in projects section

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -9,6 +9,7 @@ import { CardBody, CardContainer, CardItem } from './ui/3d-card';
 import pottymap from '../assets/pottymap.png'
 import sunnyweather from '../assets/sunnyweather.png'
 import pokedex from '../assets/pokedex.png'
+import allforone from '../assets/allforone.png'
 
 import { FaReact, FaHtml5, FaBootstrap, FaFigma, FaGithub, FaSlack } from "react-icons/fa6";
 import { IoLogoReact, IoLogoHtml5, IoLogoCss3, IoLogoJavascript, IoLogoVercel } from "react-icons/io5";
@@ -435,6 +436,70 @@ const Portfolio = () => {
                             </div>
                         </CardBody>
                     </CardContainer>
+
+                    {/* <CardContainer className="inter-var">
+                        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                            <CardItem
+                                translateZ="50"
+                                className="text-xl font-bold text-neutral-600 dark:text-white"
+                            >
+                                All For One
+                            </CardItem>
+                            <CardItem
+                                as="p"
+                                translateZ="60"
+                                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                            >
+                                Utilized React with Tailwind alongside a custom API developed using C#, .NET 7.0, and Azure Cloud Services to create a responsive and interactive website, offering users seamless interaction with various endpoints.
+                            </CardItem>
+                            <CardItem translateZ="100" className="w-full mt-4">
+                                <Image
+                                    src={allforone}
+                                    height="1000"
+                                    width="1000"
+                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                    alt="thumbnail"
+                                />
+                            </CardItem>
+                            <CardItem translateZ="60" className="w-full mt-4">
+                                <div className='grid grid-flow-col'>
+                                    <IconContext.Provider value={{ size: '50' }}>
+                                        <div>
+                                            <FaReact className='mx-auto w-10' />
+                                        </div>
+                                    </IconContext.Provider>
+                                    <IconContext.Provider value={{ size: '50' }}>
+                                        <div>
+                                            <IoLogoJavascript className='mx-auto w-10' />
+                                        </div>
+                                    </IconContext.Provider>
+                                    <IconContext.Provider value={{ size: '50' }}>
+                                        <div>
+                                            <SiTailwindcss className='mx-auto w-10' />
+                                        </div>
+                                    </IconContext.Provider>
+                                </div>
+                            </CardItem>
+                            <div className="flex justify-between items-center mt-10">
+                                <CardItem
+                                    translateZ={20}
+                                    as={Link}
+                                    href="https://packard-j-all-for-one-build.vercel.app/"
+                                    target="__blank"
+                                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                                >
+                                    Check it out →
+                                </CardItem>
+                                <CardItem
+                                translateZ={20}
+                                as="button"
+                                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                            >
+                                Sign up
+                            </CardItem>
+                            </div>
+                        </CardBody>
+                    </CardContainer> */}
                 </div>
             </section>
 
